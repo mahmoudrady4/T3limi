bad12c842ad7822cdd22deda09074274dd17230b
mahmoudrady4 <mahmoudrady5181@gmail.com>
2025-04-07 12:54:39 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => tab.classList.add('hidden'));
+  
+    document.getElementById(tabId).classList.remove('hidden');
+  }
+  
